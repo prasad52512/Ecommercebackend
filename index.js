c2e288f99ca02b8957a74408301b61c8730c5ea9
prasad52512 <35742658+prasad52512@users.noreply.github.com>
2021-04-05 00:31:57 +0530

    Add files via upload
    this backend application is used for both youtube clone app and ecommerce site

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app= express();
+var cors= require('cors');
+
+var DB= require('./config/db.config');
+var Config=require('./config/app.config');
+var UserRouter= require('./routes/user.route');
+var ProductRouter= require('./routes/product.route');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+DB.connect();
+
+app.use(cors());
+app.use('/user',UserRouter);
+app.use('/admin',ProductRouter);
+
+app.listen(Config.config.PORT, () => {
+    console.log("server started on port no  "+Config.config.PORT);
+})
